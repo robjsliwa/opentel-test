a22ccefbae5c33840fbe4fa02c24032dfe18b7a1
robjsliwa <robjsliwa@gmail.com>
2020-12-14 18:55:11 -0500

    Send traces across all components.

--- client/main.go
+++ client/main.go
@@ -2,33 +2,73 @@ package main
 
 import (
 	"context"
-	"log"
 	"fmt"
+	"log"
 
-	"opentel-test/nameserver/proto"
+	name "opentel-test/nameserver/proto"
+
+	"go.opentelemetry.io/otel/api/global"
+	"go.opentelemetry.io/otel/api/kv"
+	"go.opentelemetry.io/otel/api/trace"
+	"go.opentelemetry.io/otel/exporters/trace/jaeger"
+	"go.opentelemetry.io/otel/plugin/grpctrace"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+
 	"google.golang.org/grpc"
 )
 
 func main() {
-	cc, err := grpc.Dial("[::1]:60000", grpc.WithInsecure())
+	tp := initTraceProvider("SidekickNameClient")
+	defer tp()
+
+	tracer := global.Tracer("SidekickNameClient")
+	cc, err := grpc.Dial("[::1]:60000", grpc.WithInsecure(), grpc.WithUnaryInterceptor(grpctrace.UnaryClientInterceptor(tracer)))
 
 	if err != nil {
 		log.Fatalf("Connection error: %v", err)
 	}
 
+	defer cc.Close()
+
 	c := name.NewNameClient(cc)
-	getNickname(c)
+	getNickname(c, tracer)
 }
 
-func getNickname(c name.NameClient) {
+func initTraceProvider(service string) func() {
+	_, flush, err := jaeger.NewExportPipeline(
+		jaeger.WithCollectorEndpoint("http://localhost:14268/api/traces"),
+		jaeger.WithProcess(jaeger.Process{
+			ServiceName: service,
+			Tags: []kv.KeyValue{
+				kv.Key("exporter").String("jaeger"),
+			},
+		}),
+		jaeger.RegisterAsGlobal(),
+		jaeger.WithSDK((&sdktrace.Config{DefaultSampler: sdktrace.AlwaysSample()})),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return func() {
+		flush()
+	}
+}
+
+func getNickname(c name.NameClient, tracer trace.Tracer) {
 	ctx := context.Background()
+	ctx, span := tracer.Start(ctx, "GetSidekickNickname")
+	defer span.End()
 	
 	req := &name.NameRequest {}
 	res, err := c.GetName(ctx, req)
 	if err != nil {
+		span.RecordError(ctx, err)
 		log.Fatal("Could not get nickname.")
 		return
 	}
 
+	span.AddEvent(ctx, "Response", kv.String("SidekickName", res.Name))
 	fmt.Printf("Nickname: %v\n", res.Name)
 }
--- nameserver/src/main.rs
+++ nameserver/src/main.rs
@@ -4,27 +4,58 @@ use tonic::{transport::Server, Request, Response, Status};
 use name::name_server::{Name, NameServer};
 use name::{NameResponse, NameRequest};
 use std::collections::HashMap;
+use hyper::{Method, Client, Body};
+use opentelemetry::global;
+use opentelemetry::sdk::propagation::TraceContextPropagator;
+use opentelemetry::{
+    trace::{Span, TraceContextExt, Tracer},
+    Context, KeyValue,
+};
 
 #[derive(Default)]
 pub struct SidekickNames {}
 
 #[tonic::async_trait]
 impl Name for SidekickNames {
-    async fn get_name(&self, _: Request<NameRequest>) -> Result<Response<NameResponse>, Status> {
-        let resp = reqwest::get("http://localhost:8000/name")
+    async fn get_name(&self, request: Request<NameRequest>) -> Result<Response<NameResponse>, Status> {
+        let parent_cx = global::get_text_map_propagator(|prop| prop.extract(request.metadata()));
+        let span = global::tracer("get_name").start_from_context("get_name", &parent_cx);
+        let cx = Context::current_with_span(span);
+
+        let mut headers = reqwest::header::HeaderMap::new();
+        global::get_text_map_propagator(|propagator| {
+            propagator.inject_context(&cx, &mut headers)
+        });
+
+        println!("TEST {:?}", headers);
+        let client = reqwest::Client::new();
+        let resp = client.get("http://localhost:8000/name")
+            .headers(headers)
+            .send()
             .await
             .map_err(|e| Status::unknown(format!("{}", e)))?
             .json::<HashMap<String, String>>()
             .await
             .map_err(|e| Status::unknown(format!("{}", e)))?;
+
+        // span.set_attribute(KeyValue::new("request", format!("{:?}", resp.get(&"name".to_string()).unwrap().clone())));
+
         Ok(Response::new(NameResponse{
             name: resp.get(&"name".to_string()).unwrap().clone(),
         }))
     }
 }
 
+fn tracing_init() -> Result<(impl Tracer, opentelemetry_jaeger::Uninstall), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    global::set_text_map_propagator(TraceContextPropagator::new());
+    opentelemetry_jaeger::new_pipeline()
+        .with_service_name("nameserver")
+        .install()
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    let _uninstall = tracing_init()?;
     let addr = "[::1]:60000".parse().unwrap();
 
     let sidekick_name_server = SidekickNames::default();
